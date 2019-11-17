0a9fe122874d6d94f0c782f945891497732c96e3
unknown <jh2song@naver.com>
2019-11-17 17:20:40 +0900

    Post add & edit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     # views.post_list 함수는 이제 DB에서 필요한 데이터를 가져와서
@@ -8,3 +10,39 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # 출판 일자가 지금보다 이전으로 들어있는 행만 검색
     return render(request, 'blog/post_list.html', {'posts': posts})
     # render() 험수를 호출하여 화면을 출력, request: 함수에게 사용자가 요청한 정보를 전달, 화면출력 주체지정, 화면출력에 사용할 데이터 전달
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+#  글이 저장이 안됨
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
